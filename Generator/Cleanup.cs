c90a99063d226869332e46075dca38d58f655371
Audrey Castillo <valax321@gmail.com>
2025-09-07 23:42:43 +1000

    Add Remote storage enums to Cleanup.Expose

--- Generator/Cleanup.cs
+++ Generator/Cleanup.cs
@@ -156,6 +156,8 @@ public static class Cleanup
 		if ( name == "TimelineGameMode" ) return "public";
 		if ( name == "TimelineEventClipPriority" ) return "public";
 		if ( name == "TimelineEventHandle" ) return "public";
+		if ( name == "RemoteStorageLocalFileChange" ) return "public";
+		if ( name == "RemoteStorageFilePathType" ) return "public";
 
 		return "internal";
 	}
